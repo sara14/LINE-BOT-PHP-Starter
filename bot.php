a8da39898db3d4b6c97d6642dd274363d7c067bf
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-19 16:35:26 +0700

    no message

--- bot.php
+++ bot.php
@@ -50,7 +50,33 @@ if (!is_null($events['events'])) {
 					'text' => $userId . ":" . $text
 				];
 			}else if($text == "Profile"){
-				
+				$messages = [
+							  'type' => 'template',
+							  'altText' => 'this is a buttons template',
+							  'template' => [
+								  'type' => 'buttons',
+								  'thumbnailImageUrl' => 'https://blog.prepscholar.com/hs-fs/hubfs/main_register.jpg',
+								  'title' => 'Menu',
+								  'text' => 'Please select',
+								  'actions' => [
+									  [
+										'type' => 'postback',
+										'label'=> 'Buy',
+										'data' => 'action=buy&itemid=123'
+									  ],
+									  [
+										'type' => 'postback',
+										'label' => 'Add to cart',
+										'data' => 'action=add&itemid=123'
+									  ],
+									  [
+										'type' => 'uri',
+										'label' => 'View detail',
+										'uri' => 'http://php.fnlist.com/php/json_decode'
+									  ]
+								  ]
+							  ]
+							];
 				
 			}
 			
