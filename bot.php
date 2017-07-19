141fac13a649f5dfca0754218243c3e73c6aee28
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-19 16:12:46 +0700

    no message

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			//	'type' => 'text',
 			//	'text' => $userId . ":" . $text
 			//];
-			if($text == "confirm"){
+			if($text == "Confirm"){
 				$messages =[
 				  'type' => 'template',
 				  'altText' => 'this is a confirm template',
@@ -44,10 +44,38 @@ if (!is_null($events['events'])) {
 					  ]
 				  ]
 				];
-			}else if($text == "test"){
+			}else if($text == "Test"){
 				$messages = [
 					'type' => 'text',
 					'text' => $userId . ":" . $text
+				];
+			}else if($text == "Profile"){
+				$messages = [
+				  'type' => 'template',
+				  'altText' => 'this is a buttons template',
+				  'template' => [
+					  'type' => 'buttons',
+					  'thumbnailImageUrl' => 'http://dl.profile.line-cdn.net/0m0ed0c0367251df907a4f120f062b2235533d99806082',
+					  'title' => 'Menu',
+					  'text' => 'Please select',
+					  'actions' => [
+						  [
+							'type' => 'postback',
+							'label' => 'Buy',
+							'data' => 'action=buy&itemid=123'
+						  ],
+						  [
+							'type' => 'postback',
+							'label' => 'Add to cart',
+							'data' => 'action=add&itemid=123'
+						  ],
+						  [
+							'type' => 'uri',
+							'label' => 'View detail',
+							'uri' => 'http://example.com/page/123'
+						  ]
+					  ]
+				  ]
 				];
 			}
 			
