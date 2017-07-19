83204d44c8989f3edab7b2c4d5cbccd6b0250897
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-19 17:00:11 +0700

    no message

--- bot.php
+++ bot.php
@@ -34,12 +34,12 @@ if (!is_null($events['events'])) {
 						  [
 							'type' => 'message',
 							'label' => 'Yes',
-							'text' => 'yes'
+							'text' => 'Chose yes'
 						  ],
 						  [
 							'type' => 'message',
 							'label' => 'No',
-							'text' => 'no'
+							'text' => 'Chose no'
 						  ]
 					  ]
 				  ]
@@ -79,6 +79,41 @@ if (!is_null($events['events'])) {
 							];
 				
 			}
+			
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}else{
+			// Get text sent
+			$text = $event['type'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Get userId
+			$userId = $event['source']['userId'];
+
+			// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => $userId . ":" . $text
+				];
+
 			
 
 			// Make a POST Request to Messaging API to reply to sender
