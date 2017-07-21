61ccc73dde1c1fd4640b47e7d0347619130261d6
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-21 11:42:28 +0700

    no message

--- bot.php
+++ bot.php
@@ -80,8 +80,8 @@ if (!is_null($events['events'])) {
 							  'baseUrl' => 'https://blog.prepscholar.com/hs-fs/hubfs/main_register.jpg',
 							  'altText' => 'this is an imagemap',
 							  'baseSize' => [
-								  'height' => 259,
-								  'width' => 390
+								  'height' => 1040,
+								  'width' => 1040
 							  ],
 							  'actions' => [
 								  [
@@ -90,18 +90,18 @@ if (!is_null($events['events'])) {
 									  'area' => [
 										  'x' => 0,
 										  'y' => 0,
-										  'width' => 390,
-										  'height' => 259
+										  'width' => 1040,
+										  'height' => 1040
 									  ]
 								  ],
 								  [
 									  'type' => 'message',
 									  'text' => 'Confirm',
 									  'area' => [
-										  'x' => 160,
+										  'x' => 520,
 										  'y' => 0,
-										  'width' => 390,
-										  'height' => 259
+										  'width' => 1040,
+										  'height' => 1040
 									  ]
 								  ]
 							  ]
@@ -153,33 +153,11 @@ if (!is_null($events['events'])) {
 					'type' => 'text',
 					'text' => "Respond :" . $text
 				];
-
 			
 			replyToUser($replyToken,$messages,$access_token);
 			
-
-			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			//$data = [
-			//	'replyToken' => $replyToken,
-			//	'messages' => [$messages],
-			//];
-			//$post = json_encode($data);
-			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			//$ch = curl_init($url);
-			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//$result = curl_exec($ch);
-			//curl_close($ch);
-
-			//echo $respond . "\r\n";
 		}
 	}
 }
-isRegistered('U043c1dbc5506079b0b11d3f402aea555');
 echo "OK";
 
