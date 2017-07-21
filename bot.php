2ddd49edbf862168404ea855ee81282ff3618429
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-21 11:28:34 +0700

    no message

--- bot.php
+++ bot.php
@@ -24,7 +24,6 @@ function replyToUser($reToken,$message,$ac_token){
 	$result = curl_exec($ch);
 	curl_close($ch);
 	echo $result . "\r\n";
-	//return $result;
 }
 
 $access_token = 'Mq2pK5XVmM83DUMUh/55lt5oFCV2PzEpGV3qoG7tr/2B6MXhmOtjVwPXyfhgH27GrC8nysA0Po3KH+b+ImCfK9fg+xwPFTuLCauttOjLE47vlGSxItWqNJXLUS0xjkUnXXjblop8wg1wOocI6ezgQAdB04t89/1O/w1cDnyilFU=';
@@ -47,12 +46,6 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			
-
-			// Build message to reply back
-			//$messages = [
-			//	'type' => 'text',
-			//	'text' => $userId . ":" . $text
-			//];
 			if($text == "Confirm"){
 				$messages =[
 				  'type' => 'template',
@@ -74,12 +67,14 @@ if (!is_null($events['events'])) {
 					  ]
 				  ]
 				];
-			}else if($text == "ทดสอบ"){
-				//$messages = [
-				//	'type' => 'text',
-				//	'text' => $userId . ":" . $text
-				//];
 				
+			}else if($text == "test"){
+				$messages = [
+					'type' => 'text',
+					'text' => $userId . ":" . $text
+				];
+				
+			}else if($text == "ทดสอบ"){
 				$messages = [
 							  'type' => 'imagemap',
 							  'baseUrl' => 'https://blog.prepscholar.com/hs-fs/hubfs/main_register.jpg',
@@ -111,6 +106,7 @@ if (!is_null($events['events'])) {
 								  ]
 							  ]
 							];
+							
 			}else if($text == "Profile"){
 				$messages = [
 							  'type' => 'template',
@@ -142,27 +138,27 @@ if (!is_null($events['events'])) {
 				
 			}
 			
-			replyToUser($replyToken,$messages,$access_token);
+			//replyToUser($replyToken,$messages,$access_token);
 
 			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			//$data = [
-			//	'replyToken' => $replyToken,
-			//	'messages' => [$messages],
-			//];
-			//$post = json_encode($data);
-			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			//$ch = curl_init($url);
-			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//$result = curl_exec($ch);
-			//curl_close($ch);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-			//echo $respond . "\r\n";
+			//echo $result . "\r\n";
 		}else{
 			// Get type sent
 			$text = $event['type'] . $event['postback']['data'];
