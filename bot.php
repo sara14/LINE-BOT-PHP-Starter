513d4ff1c27bbcaec12d58ab131db268c7491fb2
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-21 11:31:52 +0700

    no message

--- bot.php
+++ bot.php
@@ -140,25 +140,6 @@ if (!is_null($events['events'])) {
 			
 			replyToUser($replyToken,$messages,$access_token);
 
-			// Make a POST Request to Messaging API to reply to sender
-			//$url = 'https://api.line.me/v2/bot/message/reply';
-			//$data = [
-			//	'replyToken' => $replyToken,
-			//	'messages' => [$messages],
-			//];
-			//$post = json_encode($data);
-			//$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			//$ch = curl_init($url);
-			//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			//$result = curl_exec($ch);
-			//curl_close($ch);
-
-			//echo $result . "\r\n";
 		}else{
 			// Get type sent
 			$text = $event['type'] . $event['postback']['data'];
@@ -174,7 +155,7 @@ if (!is_null($events['events'])) {
 				];
 
 			
-			//$respond = reply($replyToken,$messages);
+			replyToUser($replyToken,$messages,$access_token);
 			
 
 			// Make a POST Request to Messaging API to reply to sender
