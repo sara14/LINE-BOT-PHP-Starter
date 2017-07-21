e4351738c108591e8469e3055358652e9e75c655
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-21 14:42:45 +0700

    no message

--- bot.php
+++ bot.php
@@ -77,7 +77,7 @@ if (!is_null($events['events'])) {
 			}else if($text == "ทดสอบ"){
 				$messages = [
 							  'type' => 'imagemap',
-							  'baseUrl' => 'https://github.com/sara14/LINE-BOT-PHP-Starter/tree/master/image/map1/1040',
+							  'baseUrl' => 'https://github.com/line/line-bot-sdk-go/tree/master/examples/kitchensink/static/rich',
 							  'altText' => 'this is an imagemap',
 							  'baseSize' => [
 								  'height' => 1040,
