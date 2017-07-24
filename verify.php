6c1e5badc503b479e2f8157313d769f002ade0ff
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-24 09:50:23 +0700

    no message

--- verify.php
+++ verify.php
@@ -15,6 +15,6 @@ curl_close($ch);
 echo $result;
 test();
 function test(){
-	$matches = ereg('(สอบถาม)(ประกัน)', 'สอบถามข้อมูลแผนประกัน');
-	print_r('57' . $matches);
+	$matches = ereg('/(สอบถาม)(ประกัน)/', 'สอบถามข้อมูลแผนประกัน');
+	print_r('58' . $matches);
 }
