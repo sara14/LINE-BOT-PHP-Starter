1dfc64514116faa52103ba78a3f0fa1806eee8df
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-24 15:32:31 +0700

    no message

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+    require_once 'config.php';
+	try {
+		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$oStmt = $oConn->prepare('SELECT query FROM `query_table`');
+		$oStmt->execute();
+		$oResult = $oStmt->fetchAll();
+		foreach ($oResult as $aRow) {
+			print_r($aRow['data']);
+		}
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
+?> 
