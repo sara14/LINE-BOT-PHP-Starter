454684a78859fcc5821c5c9ea7d57b9fab36d8a0
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 16:06:26 +0700

    no message

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
     require_once 'config.php';
 	try {
-		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
-		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$oStmt = $oConn->prepare('SELECT iquery FROM heroku_c567de8b5a4ca4f.query_table');
-		$oStmt->execute();
-		$oResult = $oStmt->fetchAll();
-		foreach ($oResult as $aRow) {
-			print_r($aRow['iquery']);
-		}
-		$oConn=null;
+		//$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
+		//$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$oStmt = $oConn->prepare('SELECT iquery FROM heroku_c567de8b5a4ca4f.query_table');
+		//$oStmt->execute();
+		//$oResult = $oStmt->fetchAll();
+		//foreach ($oResult as $aRow) {
+		//	print_r($aRow['iquery']);
+		//}
+		//$oConn=null;
 		echo "Res:" . isRegistered('U043c1dbc5506079b0b11d3f402aea555');
 	} catch(PDOException $e) {
 		echo 'ERROR: ' . $e->getMessage();
