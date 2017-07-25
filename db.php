f118aed451335dd298d787b505580c5df11b8519
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 16:55:56 +0700

    no message

--- db.php
+++ db.php
@@ -1,18 +1,19 @@
 <?php
-    require_once 'config.php';
-	try {
-		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
-		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$oStmt = $oConn->prepare('SELECT userid FROM heroku_c567de8b5a4ca4f.user_list');
-		$oStmt->execute();
-		$oResult = $oStmt->fetchAll();
-		foreach ($oResult as $aRow) {
-			print_r($aRow['userid']);
+	require_once 'config.php';
+	echo isRegstered('U043c1dbc5506079b0b11d3f402aea555');
+	function isRegstered($usid){
+		try {
+			$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
+			$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$oStmt = $oConn->prepare('SELECT userid FROM heroku_c567de8b5a4ca4f.user_list');
+			$oStmt->execute();
+			$oResult = $oStmt->fetchAll();
+			foreach ($oResult as $aRow) {
+				print_r($aRow['userid']);
+			}
+			$oConn=null;
+		} catch(PDOException $e) {
+			echo 'ERROR: ' . $e->getMessage();
 		}
-		$oConn=null;
-		echo "Res:" . isRegistered('U043c1dbc5506079b0b11d3f402aea555');
-	} catch(PDOException $e) {
-		echo 'ERROR: ' . $e->getMessage();
 	}
-	
 ?> 
