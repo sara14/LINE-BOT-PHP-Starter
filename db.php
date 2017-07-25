6417818a27786f14288e58f589bb624c52df26b0
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 17:04:59 +0700

    no message

--- db.php
+++ db.php
@@ -16,6 +16,29 @@
 			echo 'ERROR: ' . $e->getMessage();
 		}
 	}
-	echo "" . isRegstered('U043c1dbc5506079b0b11d3f402aea555');
+	function isRegist($userId){
+	require_once 'config.php';
+	try {
+		$result = false;
+		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
+		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$oStmt = $oConn->prepare('SELECT userid FROM heroku_c567de8b5a4ca4f.user_list WHERE userid="' . $userId . '"');
+		$oStmt->execute();
+		$oResult = $oStmt->fetchAll();
+		foreach ($oResult as $aRow) {
+			if($userId == $aRow['userid']){
+				$result=true;
+			}
+		}
+		$oConn=null;
+		return $result;
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+		$result=false;
+	}
+	
+	return $result;
+}
+	echo "" . isRegist('U043c1dbc5506079b0b11d3f402aea555');
 	
 ?> 
