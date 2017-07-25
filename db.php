dfc4e1b1570950d65ce9b8715e984b5663d63de4
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 14:13:46 +0700

    no message

--- db.php
+++ db.php
@@ -1,16 +1,29 @@
 <?php
     require_once 'config.php';
 	try {
-		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
-		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$oStmt = $oConn->prepare('SELECT iquery FROM heroku_c567de8b5a4ca4f.query_table');
-		$oStmt->execute();
-		$oResult = $oStmt->fetchAll();
-		foreach ($oResult as $aRow) {
-			print_r($aRow['iquery']);
-		}
-		$oConn->close();
-	} catch(PDOException $e) {
+		//$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+		//$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$oStmt = $oConn->prepare('SELECT iquery FROM heroku_c567de8b5a4ca4f.query_table');
+		//$oStmt->execute();
+		//$oResult = $oStmt->fetchAll();
+		//foreach ($oResult as $aRow) {
+		//	print_r($aRow['iquery']);
+		//}
+		//$oConn->close
+		
+		$objConnect = mysql_connect($sHost,$sUsername,$sPassword) or die("Error Connect to Database");
+		$objDB = mysql_select_db($sDb);
+		mysql_query("SET character_set_results=tis620");
+		mysql_query("SET character_set_client=tis620");
+		mysql_query("SET character_set_connection=tis620");
+		$strSQL = 'INSERT INTO query_table VALUES("' . $replyToken . '","' . $text . '","' . $userId . '",Now())';
+		mysql_query($strSQL);
+		mysql_close($objConnect);
+	//} catch(PDOException $e) {
+	//	echo 'ERROR: ' . $e->getMessage();
+	//}
+	
+	} catch(Exception $e) {
 		echo 'ERROR: ' . $e->getMessage();
 	}
 ?> 
