c688209c3cea43eeb436b0d639638164b1940bb9
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 15:24:37 +0700

    no message

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'config.php';
 	try {
-		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+		$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb.';charset=utf8', $sUsername, $sPassword);
 		$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$oStmt = $oConn->prepare('SELECT iquery FROM heroku_c567de8b5a4ca4f.query_table');
 		$oStmt->execute();
