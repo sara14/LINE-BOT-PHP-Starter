a610b0dee3a3ac7e1f0a1fdc6029815ac1a58bdf
Sarawuth <sarawuth.sonnum@wdc.com>
2017-07-25 09:00:42 +0700

    no message

--- bot.php
+++ bot.php
@@ -36,9 +36,7 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	// Get userId
-	$userId = $event['source']['userId'];
-	if(isRegistered($userId)){
+	
 		// Loop through each event
 		foreach ($events['events'] as $event) {
 			// Reply only when message sent is in 'text' format
@@ -47,101 +45,111 @@ if (!is_null($events['events'])) {
 				$text = $event['message']['text'];
 				// Get replyToken
 				$replyToken = $event['replyToken'];
+				// Get userId
+				$userId = $event['source']['userId'];
+				if(isRegistered($userId)==true){
 				
-				if($text == "Confirm"){
-					$messages =[
-					  'type' => 'template',
-					  'altText' => 'this is a confirm template',
-					  'template' => [
-						  'type' => 'confirm',
-						  'text' => 'Are you sure?',
-						  'actions' => [
-							  [
-								'type' => 'message',
-								'label' => 'Yes',
-								'text' => 'Chose yes'
-							  ],
-							  [
-								'type' => 'message',
-								'label' => 'No',
-								'text' => 'Chose no'
+					if($text == "Confirm"){
+						$messages =[
+						  'type' => 'template',
+						  'altText' => 'this is a confirm template',
+						  'template' => [
+							  'type' => 'confirm',
+							  'text' => 'Are you sure?',
+							  'actions' => [
+								  [
+									'type' => 'message',
+									'label' => 'Yes',
+									'text' => 'Chose yes'
+								  ],
+								  [
+									'type' => 'message',
+									'label' => 'No',
+									'text' => 'Chose no'
+								  ]
 							  ]
 						  ]
-					  ]
-					];
-					
-				}else if($text == "test"){
-					$messages = [
-						'type' => 'text',
-						'text' => $userId . ":" . $text
-					];
-					
-				}else if($text == "ทดสอบ"){
-					$messages = [
-								  'type' => 'imagemap',
-								  'baseUrl' => 'https://github.com/line/line-bot-sdk-go/tree/master/examples/kitchensink/static/rich',
-								  'altText' => 'this is an imagemap',
-								  'baseSize' => [
-									  'height' => 700,
-									  'width' => 700
-								  ],
-								  'actions' => [
-									  [
-										  'type' => 'uri',
-										  'linkUri' => 'https://example.com/',
-										  'area' => [
-											  'x' => 0,
-											  'y' => 0,
-											  'width' => 700,
-											  'height' => 700
-										  ]
+						];
+						
+					}else if($text == "test"){
+						$messages = [
+							'type' => 'text',
+							'text' => $userId . ":" . $text
+						];
+						
+					}else if($text == "ทดสอบ"){
+						$messages = [
+									  'type' => 'imagemap',
+									  'baseUrl' => 'https://github.com/line/line-bot-sdk-go/tree/master/examples/kitchensink/static/rich',
+									  'altText' => 'this is an imagemap',
+									  'baseSize' => [
+										  'height' => 700,
+										  'width' => 700
 									  ],
-									  [
-										  'type' => 'message',
-										  'text' => 'Confirm',
-										  'area' => [
-											  'x' => 350,
-											  'y' => 0,
-											  'width' => 700,
-											  'height' => 700
-										  ]
-									  ]
-								  ]
-								];
-								
-				}else if($text == "Profile"){
-					$messages = [
-								  'type' => 'template',
-								  'altText' => 'this is a buttons template',
-								  'template' => [
-									  'type' => 'buttons',
-									  'thumbnailImageUrl' => 'https://blog.prepscholar.com/hs-fs/hubfs/main_register.jpg',
-									  'title' => 'ทดสอบเนื้อหา',
-									  'text' => 'Please select',
 									  'actions' => [
 										  [
-											'type' => 'postback',
-											'label'=> 'Buy',
-											'data' => 'action=buy&itemid=123'
+											  'type' => 'uri',
+											  'linkUri' => 'https://example.com/',
+											  'area' => [
+												  'x' => 0,
+												  'y' => 0,
+												  'width' => 700,
+												  'height' => 700
+											  ]
 										  ],
 										  [
-											'type' => 'postback',
-											'label' => 'Add to cart',
-											'data' => 'action=add&itemid=123'
-										  ],
-										  [
-											'type' => 'uri',
-											'label' => 'View detail',
-											'uri' => 'http://php.fnlist.com/php/json_decode'
+											  'type' => 'message',
+											  'text' => 'Confirm',
+											  'area' => [
+												  'x' => 350,
+												  'y' => 0,
+												  'width' => 700,
+												  'height' => 700
+											  ]
 										  ]
 									  ]
-								  ]
-								];
+									];
+									
+					}else if($text == "Profile"){
+						$messages = [
+									  'type' => 'template',
+									  'altText' => 'this is a buttons template',
+									  'template' => [
+										  'type' => 'buttons',
+										  'thumbnailImageUrl' => 'https://blog.prepscholar.com/hs-fs/hubfs/main_register.jpg',
+										  'title' => 'ทดสอบเนื้อหา',
+										  'text' => 'Please select',
+										  'actions' => [
+											  [
+												'type' => 'postback',
+												'label'=> 'Buy',
+												'data' => 'action=buy&itemid=123'
+											  ],
+											  [
+												'type' => 'postback',
+												'label' => 'Add to cart',
+												'data' => 'action=add&itemid=123'
+											  ],
+											  [
+												'type' => 'uri',
+												'label' => 'View detail',
+												'uri' => 'http://php.fnlist.com/php/json_decode'
+											  ]
+										  ]
+									  ]
+									];
+						
+					}
 					
+					replyToUser($replyToken,$messages,$access_token);
+				}else{
+					$replyToken = $event['replyToken'];
+					$messages = [
+									'type' => 'text',
+									'text' => 'not register';
+								];
+					replyToUser($replyToken,$messages,$access_token);
 				}
-				
-				replyToUser($replyToken,$messages,$access_token);
-
 			}else{
 				// Get type sent
 				$text = $event['type'] . $event['postback']['data'];
@@ -160,14 +168,7 @@ if (!is_null($events['events'])) {
 				
 			}
 		}
-	}else{
-		$replyToken = $event['replyToken'];
-		$messages = [
-						'type' => 'text',
-						'text' => 'คุณยังไม่ได้ลงทะเบียน กรุณาพิมพ์ "ลงทะเบียน:รหัสพนักงาน" ';
-					];
-		replyToUser($replyToken,$messages,$access_token);
-	}
+	
 }
 echo "OK";
 
