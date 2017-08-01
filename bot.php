51ac43314c36a545fad03a61c2050f7bad1a621f
Sarawuth <sarawuth.sonnum@wdc.com>
2017-08-01 11:42:08 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,11 +1,8 @@
 <?php
-$sUsername = 'b5c37df8f2ad55';
-$sPassword = 'cfa0b270';
-$sHost = 'us-cdbr-iron-east-03.cleardb.net';
-$sDb = 'heroku_c567de8b5a4ca4f';
+require_once 'config.php';
 
 function isRegistered($userId){
-	//require_once 'config.php';
+	
 	try {
 		$result = false;
 		$oConn = new PDO('mysql:host='. $GLOBALS['sHost'] .';dbname='. $GLOBALS['sDb'] .';charset=utf8', $GLOBALS['sUsername'], $GLOBALS['sPassword']);
